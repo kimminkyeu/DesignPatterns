e4f8ea20dda3c27687a0405b65dd981967ac8da2
kimminkyeu <minkyeki@student.42seoul.kr>
2023-12-13 17:50:24 +0900

    initial commit

---
+++ ex0_baseConfiguration/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Minkyeu Kim on 12/13/23.
+//
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
---
+++ ex1_assertion/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Minkyeu Kim on 12/13/23.
+//
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "hello, world!" << std::endl;
+    return 0;
+}
