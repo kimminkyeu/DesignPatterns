f9707bb3b5e27f1359a44343400783d9f070a463
kimminkyeu <minkyeki@student.42seoul.kr>
2023-12-15 19:07:04 +0900

    add ex01, 02, 03...

--- ex1_assertion/main.cpp
+++ ex1_assertion/main.cpp
@@ -11,28 +11,41 @@ class Age
 {
 private: int32_t _age;
 public: explicit Age(int32_t age) noexcept;
+
+public: Age() = delete;
+public: Age(const Age& other) = delete;
+
+public: void Set(int32_t age) noexcept;
+public: int32_t Get() const noexcept;
 };
 
-Age::Age(int32_t age) noexcept
-	: _age(age)
-{
-	MK_ASSERT_DEBUG("김민규", 0 <= age, "나이가 0보다 작을 수 없습니다.");
-}
+Age::Age(int32_t age) noexcept : _age(age) { MK_ASSERT_DEBUG("김민규", 0 <= age, "나이가 0보다 작을 수 없습니다."); }
+void Age::Set(int32_t age) noexcept { _age = age; }
+int32_t Age::Get() const noexcept { return _age; }
+
+//Age::Age(const Age& other) noexcept {};
 
 class Human
 {
 private: Age _age;
+
 public: Human(const Age& age) noexcept;
+public: Age getAge() const noexcept;
 };
 
-Human::Human(const Age& age) noexcept
-	: _age(age)
-{}
+//Human::Human(const Age& age) noexcept : _age(age) {};
+//Human::Human(const Age& age) noexcept { _age.Set(age.Get()); };
+Human::Human(const Age& age) noexcept { _age.Set(age.Get()); };
+//Age Human::getAge() const noexcept { return _age; }; // error
 
 int main(void)
 {
+//	Age _age; // error!
 //	Human jason({10}); // error!
 	Human jason(Age(10));
-	Human park(Age(-10)); // assert!
+//	Human park(Age(-10)); // assert!
+	Age jason_age = jason.getAge();
+//	Age test = jason_age;
+
     return 0;
 }
